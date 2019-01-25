ee8c635b258239254348ba7d0bba68ff92f5aefc
djalmajr <djalmajr@gmail.com>
2019-01-25 14:41:28 -0300

    updates

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.eot(\?v=\d+.\d+.\d+)?$/,
+        loader: "file-loader",
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff",
+      },
+      {
+        test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=application/octet-stream",
+      },
+      {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
         loader: "url-loader?limit=10000&mimetype=image/svg+xml",
       },
